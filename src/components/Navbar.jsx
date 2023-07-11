224016d3db8f2aebba1bd721a52ff8f7a9f58d8e
Avhira <yusupuhamka@gmail.com>
2023-07-12 05:43:13 +0700

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,8 +37,8 @@ const Navbar = () => {
         <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Avhira <span className="font-bold">Foods</span>
+        <h1 className="md:text-3xl sm:text-2xl lg:text-4xl px-2">
+          Avhira <span className="font-bold ">Foods</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black text-white rounded-full p-2">Delivery</p>
@@ -64,7 +64,7 @@ const Navbar = () => {
       <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
         <AiOutlineClose onClick={() => setNav(!nav)} size={30} className="absolute right-4 top-4 cursor-pointer" />
         <h2 className="text-2xl p-4">
-          Best <span className="font-bold">Eats</span>
+          Avhira <span className="font-bold">Foods</span>
         </h2>
         <nav>
           <ul className="flex flex-col p-4 text-gray-800">
